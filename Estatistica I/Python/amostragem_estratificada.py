88d724824c6c93f3c238378d1aebe97546bf8883
Luis Eduardo <lebarreiroj@gmail.com>
2019-01-11 00:02:34 -0200

    Mais de estatistica I com Python
    Estatistica I com Python

--- Estatistica I/Python/amostragem_estratificada.py
+++ Estatistica I/Python/amostragem_estratificada.py
@@ -1,2 +1,23 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+
+iris = pd.read_csv('..\Arquivos\iris.csv')
+iris['class'].value_counts()
+
+X, _, y, _ = train_test_split(iris.iloc[:, 0:4], 
+                              iris.iloc[:, 4], 
+                              test_size = 0.5, 
+                              stratify = iris.iloc[:, 4])
+
+y.value_counts()
+
+infert = pd.read_csv('..\Arquivos\infert.csv')
+
+infert['education'].value_counts() * 0.40
+
+X1, _, y1, _ = train_test_split(infert.iloc[:, 2:9], 
+                              infert.iloc[:, 1], 
+                              test_size = 0.61, 
+                              stratify = infert.iloc[:, 1])
+
+y1.value_counts()
