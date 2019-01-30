9196bb4641ad11d53a81acd6d61b0fa6c1062ace
Luis Eduardo Barreiro de Jesus <Luis.Jesus@mdic.gov.br>
2019-01-29 23:42:39 -0200

    Início dos exercícios de probabilidade em R

---
+++ Estatistica I/R/Probabilidade.R
@@ -0,0 +1,17 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# CURSO: FormaÃ§Ã£o Cientista de Dados com R e Python
+# INSTITUIÇÃO: UDEMY
+# ALUNO: LUIS EDUARDO
+# E-MAIL: lebarreiroj@gmail.com
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Tema: Probabilidade
+# Sub-Temas: 
+
+
+
+
+# média = 8 / dp = 2 / objeto < 6
+pnorm(6, 8, 2)
+
+qnorm(6, 8, 2)
